ebcccf22f46998659d97a8ab4a068b81222910c4
Vera Fileyeva <veremei.vera@gmail.com>
2023-01-26 16:17:18 +0000

    GuessTheFlag game added

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Vera Fileyeva on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
