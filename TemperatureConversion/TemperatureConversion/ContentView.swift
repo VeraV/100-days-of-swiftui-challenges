0f51cb9ecea153c71544f8c695cd5c5d94c85ca4
Vera Fileyeva <veremei.vera@gmail.com>
2023-01-28 19:47:51 +0000

    TemperatureConversion proj.: using enum for units

--- TemperatureConversion/TemperatureConversion/ContentView.swift
+++ TemperatureConversion/TemperatureConversion/ContentView.swift
@@ -1,33 +1,44 @@
 import SwiftUI
 
-struct ContentView: View {
-    @State private var inputUnit = "K"
-    @State private var outputUnit = "°C"
-    @State private var inputNumber = 273.15
+enum TemperatureUnit: CaseIterable {
+    case celsius, fahrenheit, kelvin
     
-    let absoluteZeroConst = 273.15
-    
-    private var outputNumber: Double {
-        let inputInKelvin: Double
-        
-        if inputUnit == "°C" {
-            inputInKelvin = inputNumber + absoluteZeroConst
-        } else if inputUnit == "°F" {
-            inputInKelvin = 5 / 9 * (inputNumber - 32) + absoluteZeroConst
-        } else {
-            inputInKelvin = inputNumber
-        }
-        
-        if outputUnit == "°C" {
-            return inputInKelvin - absoluteZeroConst
-        } else if outputUnit == "°F" {
-            return 1.8 * (inputInKelvin - absoluteZeroConst) + 32
-        } else {
-            return inputInKelvin
+    var text: String {
+        switch self {
+        case .celsius: return "°C"
+        case .fahrenheit: return "°F"
+        case .kelvin: return "K"
         }
     }
-    private let units = ["°C", "°F", "K"]
     
+    func toKelvin(_ value: Double) -> Double {
+        switch self {
+        case .celsius: return value + absoluteZeroConst
+        case .fahrenheit: return 5 / 9 * (value - 32) + absoluteZeroConst
+        case .kelvin: return value
+        }
+    }
+    
+    func fromKelvin(_ value: Double) -> Double {
+        switch self {
+        case .celsius: return value - absoluteZeroConst
+        case .fahrenheit: return 1.8 * (value - absoluteZeroConst) + 32
+        case .kelvin: return value
+        }
+    }
+}
+
+private let absoluteZeroConst = 273.15
+
+struct ContentView: View {
+    @State private var inputUnit: TemperatureUnit = .kelvin
+    @State private var outputUnit: TemperatureUnit = .celsius
+    @State private var inputNumber = 273.15
+    
+    private var outputNumber: Double {
+        let inputInKelvin: Double = inputUnit.toKelvin(inputNumber)
+        return outputUnit.fromKelvin(inputInKelvin)
+    }
     
     var body: some View {
         NavigationView {
@@ -35,16 +46,16 @@ struct ContentView: View {
                 Section {
                     TextField("Number to convert", value: $inputNumber, format: .number)
                     Picker("Convert from", selection: $inputUnit) {
-                        ForEach(units, id: \.self) {
-                            Text($0)
+                        ForEach(TemperatureUnit.allCases, id: \.self) {
+                            Text($0.text)
                         }
                     }
                 }
                 
                 Section {
                     Picker("Convert to", selection: $outputUnit) {
-                        ForEach(units, id: \.self) {
-                            Text($0)
+                        ForEach(TemperatureUnit.allCases, id: \.self) {
+                            Text($0.text)
                         }
                     }
                 }
