94d47e99ec43af84baea9ac5e6aec64e0772bb0e
Vera Fileyeva <veremei.vera@gmail.com>
2023-02-16 15:57:55 +0000

    BetterRest project added.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Vera Fileyeva on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
